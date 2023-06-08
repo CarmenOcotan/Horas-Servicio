d0693ce700a282170e0ecee404f483581009e2fd
Carmen Nuñez <liz.ocotan.clno@gmail.com>
2023-06-07 20:44:27 -0600

    "test"

--- src/componentes/Nav.js
+++ src/componentes/Nav.js
@@ -1,31 +1,34 @@
-import React from "react";
 import '../App.css';
 import logo from '../asset/logo2023.png'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
-
-function Navb (){
-    return(        
-    <Nav className="navbar fixed-top navbar-light bg-white navbar-expand shadow" aria-label="Navegación del sitio">
-            <div className="primary-navigation">
-                <div className="moremenu  d-flex navigation observed">
-                    <a href="#p" className="navbar-brand d-none d-md-flex align-items-center m-0 mr-4 p-0 aabtn">
-                        <img src={logo} className="logo" alt="Funval Internacional"/>
-                    </a>
-                    <ul className="nav more-nav navbar-nav">
-                        <li className="nav-item" role="none" data-forceintomoremenu="false">
-                            <a role="menuitem" className="nav-link  " href="#p" tabIndex="0" aria-current="true" disabled>Página Principal</a>
-                        </li>
-                        <li className="nav-item" role="none" data-forceintomoremenu="false">
-                            <a role="menuitem" className="nav-link  " href="#p" tabIndex="-1" disabled>Área personal</a>
-                        </li>
-                        <li className="nav-item" role="none" data-forceintomoremenu="false">
-                            <a role="menuitem" className="nav-link  " href="#p" tabIndex="-1" disabled> Mis cursos</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-    </Nav>
-    )
+function Navb() {
+    return (
+        <Navbar className="navbar fixed-top navbar-light bg-white nav1" aria-label="Navegación del sitio"expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#"><img src={logo} className="logo" alt="Funval Internacional"/></Navbar.Brand>
+                <Navbar.Toggle aria-controls=''/>
+                <Navbar.Offcanvas placement="end">
+                <Offcanvas.Header closeButton>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                <Nav className="justify-content-start flex-grow-1 pe-3">
+                    <Nav.Link  disabled href="#action1">Página Principal</Nav.Link>
+                    <Nav.Link  disabled href="#action2">Área personal</Nav.Link>
+                    <Nav.Link  disabled href="#action1">Mis cursos</Nav.Link>
+                </Nav>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link  href="#action1">Cerrar Sesion</Nav.Link> 
+                </Nav>
+                </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
+    );
 }
+
 export default Navb;
